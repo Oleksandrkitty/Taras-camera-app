5814634b387ee0fc26b0f95f31af737a2f2abaed
Taras Chernyshenko <taras.chernyshenko@gmail.com>
2021-06-02 10:41:29 +0300

    Added basic capturing UI

--- CameraViewController.swift
+++ CameraViewController.swift
@@ -8,10 +8,20 @@
 import UIKit
 
 class CameraViewController: UIViewController {
+    @IBOutlet private weak var containerView: UIView!
+    @IBOutlet private weak var captureButton: UIButton!
+    
+    @IBAction private func cancelButtonPressed(_ button: UIButton) {
+       //TODO: dismiss screen here
+    }
+    
+    @IBAction private func cameraButtonPressed(_ button: UIButton) {
+        print("Capture")
+    }
+    
     var viewModel: CameraVM!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("Camera loaded")
     }
 }
