339aecc62a2f2acb85610501b293f36ea713d634
Taras Chernyshenko <taras.chernyshenko@gmail.com>
2021-06-03 10:28:50 +0300

    Added front flash light

--- CameraViewController.swift
+++ CameraViewController.swift
@@ -10,16 +10,45 @@ import UIKit
 class CameraViewController: UIViewController {
     @IBOutlet private weak var containerView: CameraPreviewView!
     @IBOutlet private weak var captureButton: UIButton!
-    
+    @IBOutlet private weak var flashView: UIView!
+    @IBOutlet private weak var seriesLabel: UILabel!
+
     @IBAction private func cancelButtonPressed(_ button: UIButton) {
        //TODO: dismiss screen here
+    }
+    
+    @IBAction private func lowButtonPressed(_ button: UIButton) {
+        viewModel.change(series: Low)
+    }
+    
+    @IBAction private func mediumButtonPressed(_ button: UIButton) {
+        viewModel.change(series: Medium)
+    }
+    
+    @IBAction private func highButtonPressed(_ button: UIButton) {
+        viewModel.change(series: High)
     }
     
     @IBAction private func cameraButtonPressed(_ button: UIButton) {
         viewModel.capture()
     }
     
-    var viewModel: CameraVM!
+    var viewModel: CameraVM! {
+        didSet {
+            viewModel.isFlashEnabled.bind { [unowned self] isEnabled in
+                if isEnabled {
+                    self.flashView.isHidden = false
+                } else {
+                    UIView.animate(withDuration: 0.1) {
+                        self.flashView.isHidden = true
+                    }
+                }
+            }
+            viewModel.seriesString.bind { [unowned self] string in
+                self.seriesLabel.text = string
+            }
+        }
+    }
 
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
--- camera-poc/Screens/Camera/CameraVM.swift
+++ camera-poc/Screens/Camera/CameraVM.swift
@@ -12,11 +12,35 @@ import Photos
 class CameraVM: NSObject {
     private let router: CameraRouting
     private let photoOutput = AVCapturePhotoOutput()
+    private var videoInput: AVCaptureDeviceInput?
     let captureSession: AVCaptureSession = AVCaptureSession()
+    
+    private var screenBrightness: CGFloat = 0.5
+    private(set) var isFlashEnabled: Bound<Bool> = Bound(false)
+    private(set) var flashBrightness: Bound<CGFloat> = Bound(0.0)
+    private(set) var seriesString: Bound<String> = Bound("")
+    private var series: BrigtnessSeries = Medium {
+        didSet {
+            brightness = series.min / 100
+            minBrightness = series.min / 100
+            maxBrightness = series.max / 100
+            seriesString.value = "\(series.min)% - \(series.max)%"
+            UIScreen.main.brightness = minBrightness
+        }
+    }
+
+    private var brightness: CGFloat = 0.0
+    private var minBrightness: CGFloat = 0.0
+    private var maxBrightness: CGFloat = 1.0
     
     init(router: CameraRouting) {
         self.router = router
         super.init()
+        brightness = series.min / 100
+        minBrightness = series.min / 100
+        maxBrightness = series.max / 100
+        seriesString.value = "\(series.min)% - \(series.max)%"
+        UIScreen.main.brightness = minBrightness
     }
     
     func requestCameraAccess() {
@@ -48,6 +72,7 @@ class CameraVM: NSObject {
             let videoInput = try AVCaptureDeviceInput(device: videoDevice)
             if captureSession.canAddInput(videoInput) {
                 captureSession.addInput(videoInput)
+                self.videoInput = videoInput
             }
         } catch {
             assertionFailure("Can't add video input")
@@ -61,21 +86,57 @@ class CameraVM: NSObject {
         captureSession.startRunning()
     }
     
+    func change(series: BrigtnessSeries) {
+        self.series = series
+    }
+
     func capture() {
-        let settings = AVCapturePhotoSettings()
-        settings.flashMode = .off
-        photoOutput.capturePhoto(with: settings, delegate: self)
+        brightness = minBrightness
+        makePhoto()
+    }
+    
+    private func makePhoto() {
+        if brightness > maxBrightness {
+            return
+        }
+        print("brightness \(brightness)")
+        capture(brightness: brightness)
+    }
+    
+    private func capture(brightness: CGFloat) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+            let settings = AVCapturePhotoSettings()
+            settings.flashMode = .off
+            //set starting brightness to achive flashing effect
+            UIScreen.main.brightness = max(brightness - 0.12, self.series.min / 100)
+            self.screenBrightness = max(brightness - 0.12, self.series.min / 100)
+            self.isFlashEnabled.value = true
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+                UIScreen.main.brightness = brightness
+                DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+                    self.photoOutput.capturePhoto(with: settings, delegate: self)
+                }
+            }
+        }
     }
 }
 
 extension CameraVM: AVCapturePhotoCaptureDelegate {
     func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
+        DispatchQueue.main.asyncAfter(deadline: .now()) {
+            self.isFlashEnabled.value = false
+            UIScreen.main.brightness = self.screenBrightness
+        }
+        
         PHPhotoLibrary.requestAuthorization { status in
             guard status == .authorized else { return }
             
             PHPhotoLibrary.shared().performChanges {
                 let creationRequest = PHAssetCreationRequest.forAsset()
                 creationRequest.addResource(with: .photo, data: photo.fileDataRepresentation()!, options: nil)
+                let brightness = self.brightness + self.series.step
+                self.brightness = round(brightness * 100) / 100
+                self.makePhoto()
             } completionHandler: { isSaved, error in
                 guard let error = error else { return }
                 assertionFailure("Handle saving error: \(error.localizedDescription)")
@@ -83,3 +144,13 @@ extension CameraVM: AVCapturePhotoCaptureDelegate {
         }
     }
 }
+
+struct BrigtnessSeries {
+    let min: CGFloat
+    let max: CGFloat
+    let step: CGFloat
+}
+
+let Low = BrigtnessSeries(min: 10, max: 40, step: 0.05)
+let Medium = BrigtnessSeries(min: 45, max: 70, step: 0.05)
+let High = BrigtnessSeries(min: 75, max: 100, step: 0.05)
