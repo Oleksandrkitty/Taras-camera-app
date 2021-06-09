91cdb561ec9603210dddb499a276e57df9528f38
Taras Chernyshenko <taras.chernyshenko@gmail.com>
2021-06-09 19:06:53 +0300

    Added oval overlay

--- CameraViewController.swift
+++ CameraViewController.swift
@@ -52,6 +52,8 @@ class CameraViewController: UIViewController {
         viewModel.reset()
     }
     
+    private lazy var ovalOverlayView = OvalOverlayView(frame: containerView.bounds)
+    
     private lazy var usvPicker: UIView = {
         let picker = USVPickerView(frame: .zero)
         picker.translatesAutoresizingMaskIntoConstraints = false
@@ -172,6 +174,7 @@ class CameraViewController: UIViewController {
         containerView.videoPreviewLayer.session = viewModel.session
         containerView.videoPreviewLayer.videoGravity = .resizeAspectFill
         viewModel.requestCameraAccess()
+        containerView.addSubview(ovalOverlayView)
     }
 }
 
@@ -203,4 +206,45 @@ extension CameraViewController: WhiteBalanceSliderViewDelegate {
     func temperatureValueDidChanged(_ value: Float) {
         viewModel.change(temperature: value)
     }
+}
+
+class OvalOverlayView: UIView {
+    private var overlayFrame: CGRect!
+    private let width: CGFloat = 200
+    private let height: CGFloat = 300
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = UIColor.clear
+        overlayFrame = CGRect(x: (frame.width - width) / 2,
+                              y: (frame.height - height) / 2,
+                              width: width,
+                              height: height)
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+
+    override func draw(_ rect: CGRect) {
+        let overlayPath = UIBezierPath(rect: bounds)
+        let ovalPath = UIBezierPath(ovalIn: overlayFrame)
+        overlayPath.append(ovalPath)
+        overlayPath.usesEvenOddFillRule = true
+
+        let ovalLayer = CAShapeLayer()
+        ovalLayer.path = ovalPath.cgPath
+        ovalLayer.fillColor = UIColor.clear.cgColor
+        ovalLayer.strokeColor = UIColor(red: 254 / 255, green: 212 / 255, blue: 22 / 255, alpha: 1.0).cgColor
+        ovalLayer.lineWidth = 2.0
+        
+        let fillLayer = CAShapeLayer()
+        fillLayer.path = overlayPath.cgPath
+        fillLayer.fillRule = CAShapeLayerFillRule.evenOdd
+        fillLayer.fillColor = UIColor.black.withAlphaComponent(0.5).cgColor
+        
+        layer.addSublayer(fillLayer)
+        layer.addSublayer(ovalLayer)
+    }
+
 }
